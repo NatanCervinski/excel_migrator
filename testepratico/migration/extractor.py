b18f2e3a128391d79b92f39fc46bd062225870d7
NatanCervinski <natanscer@gmail.com>
2024-12-02 01:58:42 -0300

    criada funcoes para carregamento de arquivos

--- testepratico/migration/extractor.py
+++ testepratico/migration/extractor.py
@@ -8,6 +8,8 @@ import pandas as pd
 import rarfile
 from config import settings
 
+ENCODINGS_TO_TRY = settings.MIGRATION_CONFIGS.ENCODINGS_TO_TRY
+
 
 def load_csv_from_directory(
     directory_path: str,
@@ -15,6 +17,28 @@ def load_csv_from_directory(
 ) -> Dict[str, pd.DataFrame]:
     lst_csv_files = list_csv_in_directory(directory_path)
     return load_dataframes_from_csv(directory_path, lst_csv_files, delimiter)
+
+
+def load_single_csv(
+    file_path: str,
+    delimiter: str = ";",
+) -> pd.DataFrame:
+    if not file_path.endswith(".csv"):
+        raise ValueError("O arquivo fornecido não é um arquivo CSV.")
+
+    with open(file_path, "r") as file:
+        for enc in ENCODINGS_TO_TRY:
+            try:
+                df = pd.read_csv(file, delimiter=delimiter, encoding=enc)
+                return df
+            except UnicodeDecodeError:
+                continue
+            except Exception as e:
+                logging.error(
+                    f"Erro ao carregar {file_path} com codificação {enc}: {e}"
+                )
+                break
+    return pd.DataFrame()
 
 
 def load_csv_from_compressed_file(
@@ -38,7 +62,9 @@ def load_csv_from_compressed_file(
 
 def list_csv_in_directory(directory: str) -> List[str]:
     return [
-        f for f in os.listdir(directory) if f.startswith("v_") and f.endswith(".csv")
+        f
+        for f in os.listdir(directory)
+        if f.startswith(settings.MIGRATION_CONFIGS.CSV_PREFIX) and f.endswith(".csv")
     ]
 
 
@@ -46,13 +72,13 @@ def load_dataframes_from_csv(
     directory: str, csv_files: List[str], delimiter: str
 ) -> Dict[str, pd.DataFrame]:
     dic_dataframes = {}
-    encodings_to_try = settings.ENCODINGS_TO_TRY
+    encodings_to_try = ENCODINGS_TO_TRY
 
     for csv_file in csv_files:
         file_path = os.path.join(directory, csv_file)
         for enc in encodings_to_try:
             try:
-                df = pd.read_csv(file_path, delimiter=delimiter, encoding=enc)
+                df = pd.read_csv(file_path, delimiter=delimiter, encoding=enc, header=0)
                 dic_dataframes[csv_file] = df
                 break
             except UnicodeDecodeError:
